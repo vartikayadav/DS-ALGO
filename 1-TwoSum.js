a5d83b79b338aa29ee4376cfd65b9ed857e297c5
Vartika Yadav <38458783+vartikayadav@users.noreply.github.com>
2022-08-21 12:05:10 +0530

    Add files via upload

---
+++ 1-TwoSum.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = function(nums, target) {
+    
+    let map = new Map()
+    
+    for(let i=0;i<nums.length;i++){
+        
+        map.set(nums[i],i);
+        
+        
+    }
+    
+    for(let i=0;i<nums.length;i++){
+        
+        if(map.has(target - nums[i]) && (map.get(target-nums[i])!=i)){
+            
+            return [i,map.get(target-nums[i])]
+        }
+    }
+    
+    
+    
+    
+};
