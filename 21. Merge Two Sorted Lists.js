fddd9074c604913e241d60703b1b052b7e335ce5
Vartika Yadav <38458783+vartikayadav@users.noreply.github.com>
2023-01-09 22:54:33 +0530

    Add files via upload

---
+++ 21. Merge Two Sorted Lists.js
@@ -0,0 +1,64 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+
+    tail = new ListNode(0);
+    dummyNode = tail;
+
+    if(list1 === null && list2 === null){
+        return null
+    }
+    if(list1 === null){
+        return list2
+    }
+    if(list2 === null){
+        return list1
+    }
+
+    while(true){
+
+         if(list1===null){
+        dummyNode.next = list2
+        break
+    }
+
+    if(list2 === null){
+        dummyNode.next = list1
+        break
+    }
+
+     if(list1.val<=list2.val){
+         dummyNode.next = list1
+         list1 = list1.next
+     }
+
+     else{
+         dummyNode.next = list2
+         list2 = list2.next
+     }
+
+
+   
+
+    dummyNode = dummyNode.next
+
+    }
+
+
+    return tail.next
+
+
+
+      
+    
+};
